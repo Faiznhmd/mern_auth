8edc6990e648fa5e8f010b363feb559fa4b3e53b
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-03-15 01:17:39 +0530

    routes and routes controllers

--- backend/controllers/usercontrollers.js
+++ backend/controllers/usercontrollers.js
@@ -4,4 +4,26 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Auth Users' });
 });
 
-export { authUser };
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Register Users' });
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Logout Users' });
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get Users' });
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'update Users' });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
