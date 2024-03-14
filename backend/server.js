997af6814ffad243b8be9aa1673bd108dfc164dd
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-03-15 01:17:13 +0530

    mongoDB  connected

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ import dotenv from 'dotenv';
 
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middlewares/errorMiddlewares.js';
+import connectDB from './config/db.js';
+
 dotenv.config();
+connectDB();
+
 const PORT = 5000;
 
 const app = express();
@@ -12,7 +16,6 @@ app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
-app.use(errorHandler);
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is started at ${PORT}`));
