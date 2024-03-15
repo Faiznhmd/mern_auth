beb05ceda6cf317e648960a1b10876eef28f0aab
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-03-15 22:10:32 +0530

    register,logout controllers

--- backend/controllers/usercontrollers.js
+++ backend/controllers/usercontrollers.js
@@ -1,15 +1,58 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
+//login
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Auth Users' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invaild  email or password');
+  }
 });
 
+//register
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Register Users' });
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(404);
+    throw new Error('User already Exist');
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invaild user data');
+  }
 });
+
+//logout
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Logout Users' });
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: ' User Logged Out' });
 });
 
 const getUserProfile = asyncHandler(async (req, res) => {
